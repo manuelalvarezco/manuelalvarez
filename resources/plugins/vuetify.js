6a6fe2ea046d7620d31f7188a46e3538c406b5da
manuel alvarez <alvarezbautista.luis@gmail.com>
2020-04-08 12:07:24 -0500

    first commit

---
+++ resources/plugins/vuetify.js
@@ -0,0 +1,15 @@
+// You still need to register Vuetify itself
+// src/plugins/vuetify.js
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
+import Vue from 'vue'
+import Vuetify from 'vuetify/lib'
+
+Vue.use(Vuetify)
+
+const opts = {
+    icons: {
+        iconfont: 'md',
+    },
+}
+
+export default new Vuetify(opts)
