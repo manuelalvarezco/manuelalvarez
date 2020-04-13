030d47a4162da0bd641dedccd18aa13f7b24b7e7
manuel alvarez <alvarezbautista.luis@gmail.com>
2020-04-13 11:13:39 -0500

    images

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>Biowell - @yield('title')</title>
+    <title>Biowellness - @yield('title')</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
