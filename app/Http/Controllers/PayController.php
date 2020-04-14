bd0f0466f349e8215cb3c6dd19981347288718a5
jpaz93 <juanpa.aristis@gmail.com>
2020-04-14 09:34:10 -0500

    PayU

--- app/Http/Controllers/PayController.php
+++ app/Http/Controllers/PayController.php
@@ -29,7 +29,6 @@ class PayController extends Controller
 
     public function responsePayU()
     {
-        \Log::info('entra o no entra?');
         // Indica el estado de la transacción en el sistema.
         $state_pol            = $_POST['state_pol'];
         // El código de respuesta de PayU.
