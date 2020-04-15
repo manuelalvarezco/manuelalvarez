885a2bd7e77c2567738a63e5362cecd2d71e43b0
manuel alvarez <alvarezbautista.luis@gmail.com>
2020-04-15 13:10:54 -0500

    shipingCity

--- app/PayuResponse.php
+++ app/PayuResponse.php
@@ -7,6 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class PayuResponse extends Model
 {
     protected $fillable = [
-        'state_pol', 'response_code_pol', 'reference_sale', 'reference_pol', 'payment_method', 'payment_method_type', 'value', 'tax', 'transaction_date', 'currency', 'email_buyer', 'description', 'shipping_address', 'phone', 'payment_method_id', 'response_message_pol', 'transaction_id', 'payment_method_name', 'nickname_buyer',
+        'state_pol', 'response_code_pol', 'reference_sale', 'reference_pol', 'payment_method', 'payment_method_type', 'value', 'tax', 'transaction_date', 'currency', 'email_buyer', 'description', 'shipping_city','shipping_address', 'phone', 'payment_method_id', 'response_message_pol', 'transaction_id', 'payment_method_name', 'nickname_buyer',
     ];
 }
