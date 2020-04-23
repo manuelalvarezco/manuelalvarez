e0a8cb5f9f5e370426399ce72affe58b2fc09b09
Manuel Alvarez <alvarezbautista.luis@gmail.com>
2020-04-22 20:48:29 -0500

    color nabvar

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -3,7 +3,7 @@
 
 
     <a class="navbar-brand" href="#">
-        <img src="{{ url('img/icono.png') }}" width="80" alt="">
+        <img src="{{ url('img/icono-blanco.png') }}" width="80" alt="">
       </a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
